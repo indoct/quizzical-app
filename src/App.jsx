9c68ec7b523ee7cb64da9fe087f7e011859fc897
syoban <syoban@gmail.com>
2023-10-11 23:26:46 +0100

    radio buttons attempts

--- src/App.jsx
+++ src/App.jsx
@@ -9,41 +9,38 @@ function App() {
     const [quizStarted, setQuizStarted] = React.useState(false);
     const [questions, setQuestions] = React.useState({});
 
-    function startQuiz() {
-        setQuizStarted(true)
-    }
-
   React.useEffect(() => {
       fetch('https://opentdb.com/api.php?amount=5')
           .then(res => res.json())
-          .then(data => setQuestions(data.results.map(q => {
+          .then(data => setQuestions(data.results.map((q,ind) => {
             const incArray = q.incorrect_answers;
             incArray.splice(((incArray.length+1) * Math.random()), 0, q.correct_answer);
             const decodedArr = [];
             incArray.map(item => decodedArr.push(he.decode(item)))
-            const decQ = he.decode(q.question)
+            const decQ = he.decode(q.question);
+            const qName = ind === 0 ? `question01` : `question0${ind+1}`
 
             return {
-                qId: nanoid(),
+                qId: qName,
                 question: decQ,
                 correct: he.decode(q.correct_answer),
                 answers: decodedArr,
-                [decQ]: ""
+                [qName]: ""
             }
           })))
   }, [])
 
-function handleChange(event) {
-    const {name, value, type, checked} = event.target
+function startQuiz() {
+    setQuizStarted(true)
+}
 
-//    const testObj = { [name]: type === "checkbox" ? checked : value } 
-//    console.log(testObj)
-//    return testObj
+function handleChange(event) {
+    const { name, value, type, checked } = event.target;
 
     setQuestions(questions.map(question => {
         return {
             ...question,
-            [name]: type === "checkbox" ? checked : value,
+            [name]: type === "checkbox" ? checked : value
         }
     }))
 
@@ -51,18 +48,26 @@ function handleChange(event) {
 
 const handleSubmit = (e) => {
     e.preventDefault()
+    console.log(questions)
 }
   
   const questionElements = (questions.length === undefined) ? '' : questions.map((q,ind) => { 
-        const qContent = q.question;
+    // const propName = Object.getOwnPropertyNames(q)[4];
+    // const qName = ind === 0 ? `question01` : `question0${ind+1}`
+    // console.log(q.qName)
+    // console.log(q[`'propName'`])
+    // const currentQ = q;
+    // console.log(currentQ)
+    const testObj = Object.values(q)[0];
+    console.log(testObj)
 
           return (
-            <article className="qna-container" key={q.qId}>
+            <article className="qna-container" key={nanoid()}>
               <QuestionBlock     
                 question={q.question}
+                // selected={q.propName}
                 qid={q.qId}
                 qNum={ind}
-                // selected={q[qContent]}
                 answers={q.answers}
                 handleChange={handleChange}
               />
@@ -77,7 +82,7 @@ const handleSubmit = (e) => {
               <section className="quiz-container">
                 <form onSubmit={handleSubmit}>
                     {questionElements}
-                    <button disabled>Check Answers</button>
+                    <button>Check Answers</button>
                 </form>
                     
               </section>
--- src/components/QuestionBlock.jsx
+++ src/components/QuestionBlock.jsx
@@ -6,14 +6,15 @@ export default function QuestionBlock(props) {
 const answerElements = props.answers.map((a,i) => {
     const akey = nanoid();
     const aId = `Q${props.qNum+1}0${i+1}-${a}`
+    // console.log(Object.keys(a))
 
     return (
             <label key={akey} htmlFor={aId}>
                 <input 
                     type="radio"
-                    name={props.question}
+                    name={props.qid}
                     id={aId}
-                    // checked={checked}
+                    // checked={??? what the hell goes here?}
                     value={a}
                     onChange={props.handleChange}
                 /> {a}
