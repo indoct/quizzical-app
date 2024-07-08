0851f7dda8f3b0097827fd606e2210fa8c847e0b
syoban <syoban@gmail.com>
2024-07-09 00:00:31 +0100

    netlify routing

---
+++ src/components/Header.tsx
@@ -0,0 +1,68 @@
+import { UilMoon, UilSun, UilSetting } from "@iconscout/react-unicons";
+
+interface HeaderProps {
+  isDarkMode: boolean;
+  toggleDarkMode: () => void;
+  handleNewSettings: () => void;
+}
+
+const Header: React.FC<HeaderProps> = ({
+  isDarkMode,
+  toggleDarkMode,
+  handleNewSettings,
+}) => {
+  // const [allowFetch, setAllowFetch] = useState<boolean>(false);
+
+  // useEffect(() => {
+  //   setAllowFetch(false);
+
+  //   const fetchTimer = setTimeout(() => {
+  //     setAllowFetch(true);
+  //   }, 5000);
+
+  //   return () => clearTimeout(fetchTimer);
+  // }, []);
+
+  return (
+    <header>
+      <div className="controls">
+        {/* <button
+          role="button"
+          aria-label="Get new questions"
+          className="ctrl-btn"
+          disabled={!allowFetch}
+        >
+          <UilRefresh size={28} />
+        </button> */}
+        <button
+          role="button"
+          aria-label="Change settings"
+          className="ctrl-btn"
+          onClick={handleNewSettings}
+        >
+          <UilSetting size={28} />
+        </button>
+      </div>
+      <div>
+        <h1>Quizzical</h1>
+      </div>
+      <div className="toggle">
+        <span>Switch to: &nbsp;</span>
+        <button onClick={() => toggleDarkMode()} className="toggle-btn">
+          {isDarkMode ? (
+            <>
+              <UilSun size={16} color={"#0f1422"} /> Light
+            </>
+          ) : (
+            <>
+              <UilMoon size={16} color={"#ffffff"} /> Dark
+            </>
+          )}{" "}
+          Mode
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
